7755a687339f51048f815bfbd7b30ad18f8b8463
EmpAmir <97233933+EmpAmir@users.noreply.github.com>
2022-01-10 12:41:39 +0530

    Create auto0ff.php

---
+++ auto0ff.php
@@ -0,0 +1,4 @@
+ <label class="form-label float-start">Enter Your Amount</label>
+<div class="input-group">
+ <input type="text" autocomplete="off"  class="form-control only_number" name="amount" id="amount"  onkeyup="word.innerHTML=convertNumberToWords(this.value)">
+</div>
